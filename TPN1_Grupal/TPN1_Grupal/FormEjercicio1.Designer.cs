064434a116591135afe3d436d0dd0bcd5c789621
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-03-27 20:23:07 -0300

    Version 1.01
    Le agregué un label, una barra de texto y un botón "agregar" para ir completando el ejercicio

--- TPN1_Grupal/TPN1_Grupal/FormEjercicio1.Designer.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.Designer.cs
@@ -29,20 +29,58 @@ namespace TPN1_Grupal
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAceptar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(60, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese un Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(209, 73);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(262, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.Location = new System.Drawing.Point(503, 73);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(80, 24);
+            this.btnAceptar.TabIndex = 2;
+            this.btnAceptar.Text = "Agregar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
             // 
             // FormEjercicio1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(636, 356);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label1);
             this.Name = "FormEjercicio1";
             this.Text = "Nombres";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAceptar;
     }
 }
