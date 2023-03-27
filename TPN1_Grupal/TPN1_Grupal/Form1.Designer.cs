5896667a806d1f8125767f07cad6f6a35b0d53b8
Gonzalez_Walter_UTN-FRGP <gonzalezwalter.curso2023@gmail.com>
2023-03-27 18:51:26 -0300

    Aporte TP Grupal N1

---
+++ TPN1_Grupal/TPN1_Grupal/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace TPN1_Grupal
+{
+    partial class frmVentanaPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEjercicio1 = new System.Windows.Forms.Button();
+            this.btnEjercicio2 = new System.Windows.Forms.Button();
+            this.btnEjercicio3 = new System.Windows.Forms.Button();
+            this.lblIntegrantes = new System.Windows.Forms.Label();
+            this.lblNombre1 = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEjercicio1
+            // 
+            this.btnEjercicio1.Location = new System.Drawing.Point(27, 28);
+            this.btnEjercicio1.Name = "btnEjercicio1";
+            this.btnEjercicio1.Size = new System.Drawing.Size(165, 48);
+            this.btnEjercicio1.TabIndex = 0;
+            this.btnEjercicio1.Text = "EJERCICIO1";
+            this.btnEjercicio1.UseVisualStyleBackColor = true;
+            this.btnEjercicio1.Click += new System.EventHandler(this.btnEjercicio1_Click);
+            // 
+            // btnEjercicio2
+            // 
+            this.btnEjercicio2.Location = new System.Drawing.Point(223, 28);
+            this.btnEjercicio2.Name = "btnEjercicio2";
+            this.btnEjercicio2.Size = new System.Drawing.Size(165, 48);
+            this.btnEjercicio2.TabIndex = 1;
+            this.btnEjercicio2.Text = "EJERCICIO2";
+            this.btnEjercicio2.UseVisualStyleBackColor = true;
+            this.btnEjercicio2.Click += new System.EventHandler(this.btnEjercicio2_Click);
+            // 
+            // btnEjercicio3
+            // 
+            this.btnEjercicio3.Location = new System.Drawing.Point(414, 28);
+            this.btnEjercicio3.Name = "btnEjercicio3";
+            this.btnEjercicio3.Size = new System.Drawing.Size(165, 48);
+            this.btnEjercicio3.TabIndex = 2;
+            this.btnEjercicio3.Text = "EJERCICIO3";
+            this.btnEjercicio3.UseVisualStyleBackColor = true;
+            this.btnEjercicio3.Click += new System.EventHandler(this.btnEjercicio3_Click);
+            // 
+            // lblIntegrantes
+            // 
+            this.lblIntegrantes.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIntegrantes.Location = new System.Drawing.Point(24, 106);
+            this.lblIntegrantes.Name = "lblIntegrantes";
+            this.lblIntegrantes.Size = new System.Drawing.Size(133, 31);
+            this.lblIntegrantes.TabIndex = 3;
+            this.lblIntegrantes.Text = "Integrantes";
+            // 
+            // lblNombre1
+            // 
+            this.lblNombre1.AutoSize = true;
+            this.lblNombre1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre1.Location = new System.Drawing.Point(157, 146);
+            this.lblNombre1.Name = "lblNombre1";
+            this.lblNombre1.Size = new System.Drawing.Size(161, 24);
+            this.lblNombre1.TabIndex = 4;
+            this.lblNombre1.Text = "Enzo Matías Nieto";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(157, 174);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(154, 24);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nahuel Sandoval";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Axel Mazo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(157, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(233, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Walter Alejandro González";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(157, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Matías Rocha";
+            // 
+            // frmVentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(595, 300);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblNombre1);
+            this.Controls.Add(this.lblIntegrantes);
+            this.Controls.Add(this.btnEjercicio3);
+            this.Controls.Add(this.btnEjercicio2);
+            this.Controls.Add(this.btnEjercicio1);
+            this.Name = "frmVentanaPrincipal";
+            this.Text = "Formulario Principal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEjercicio1;
+        private System.Windows.Forms.Button btnEjercicio2;
+        private System.Windows.Forms.Button btnEjercicio3;
+        private System.Windows.Forms.Label lblIntegrantes;
+        private System.Windows.Forms.Label lblNombre1;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPN1_Grupal
+{
+    public partial class FormEjercicio1 : Form
+    {
+        public FormEjercicio1()
+        {
+            InitializeComponent();
+        }
+    }
+}
