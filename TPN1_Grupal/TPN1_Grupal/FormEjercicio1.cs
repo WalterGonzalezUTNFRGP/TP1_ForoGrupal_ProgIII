159dd1b161c0c4764d6ce280fc8252e03f250653
MNRocha <mnrrocha@hotmail.com>
2023-03-28 01:44:24 -0300

    Version 0.04
    Agregué una condición al comprobar si el text box que contiene el nombre tiene ingresados solo espacios en blanco.
    
    Realicé validación de nombre repetido en el list box.
    
    agregué línea en la que se vacía el text box una vez se carga el nombre en la lista.

--- TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
@@ -19,15 +19,37 @@ namespace TPN1_Grupal
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrEmpty(txtBoxAgregar.Text))
+            if (string.IsNullOrEmpty(txtBoxAgregar.Text) || txtBoxAgregar.Text.Trim() == "")
             {
                 MessageBox.Show("Debe ingresar un nombre!");
+                txtBoxAgregar.Text = "";        /// Después de mostrar cartel deja vacío el Text Box).
+                                                /// Más que nada cuando se ingresan sólo espacios en blanco. 
             }
             else
             {
-                txtBoxAgregar.Text.Trim();
-                listNombre.Items.Add(txtBoxAgregar.Text);
-            }    
-        }
+                int cantNombres, i;
+                bool repetido = false;
+                cantNombres = listNombre.Items.Count; // Cuenta la cantidad de nombres cargados en el List Box.
+
+                for (i = 0; i < cantNombres; i++)
+                {
+                    // Compara el nombre ingresado en el text box con cada uno de los nombres del list box. Antes pasa toda la cadena de ambos nombres a MAYÚSCULAS.
+                    if (txtBoxAgregar.Text.ToUpper() == listNombre.Items[i].ToString().ToUpper())
+                    {
+                        MessageBox.Show("El nombre ingresado ya se encuentra en la lista", "ATENCIÓN");
+                        repetido = true;
+                    }
+                }
+                // Si no hay repetido agrega el nombre a la lista.
+                if (repetido == false)
+                {
+                    //txtBoxAgregar.Text.Trim();
+                    listNombre.Items.Add(txtBoxAgregar.Text.Trim());    /// Quita los espacios del inicio y final de la cadena
+                                                                        /// al momento de agregar el nombre al List Box.
+                    txtBoxAgregar.Text = "";                            /// Vuelve a poner en blanco el Text Box
+                }
+            }
+
+        }      
     }
 }
