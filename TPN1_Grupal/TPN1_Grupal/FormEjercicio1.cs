066985df6a0620361fbe13deb22b3352f3f4845a
MNRocha <mnrrocha@hotmail.com>
2023-03-28 02:36:29 -0300

    Version 0.04
    Corregí un error.

--- TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
@@ -34,7 +34,7 @@ namespace TPN1_Grupal
                 for (i = 0; i < cantNombres; i++)
                 {
                     // Compara el nombre ingresado en el text box con cada uno de los nombres del list box. Antes pasa toda la cadena de ambos nombres a MAYÚSCULAS.
-                    if (txtBoxAgregar.Text.ToUpper() == listNombre.Items[i].ToString().ToUpper())
+                    if (txtBoxAgregar.Text.ToUpper().Trim() == listNombre.Items[i].ToString().ToUpper())
                     {
                         MessageBox.Show("El nombre ingresado ya se encuentra en la lista", "ATENCIÓN");
                         repetido = true;
