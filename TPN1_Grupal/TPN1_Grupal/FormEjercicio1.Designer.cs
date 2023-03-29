31a76cfa2bc338e03c16a33b6f86afcb599def9e
Gonzalez_Walter_UTN-FRGP <gonzalezwalter.curso2023@gmail.com>
2023-03-29 06:54:37 -0300

    version 0.11

--- TPN1_Grupal/TPN1_Grupal/FormEjercicio1.Designer.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.Designer.cs
@@ -29,7 +29,86 @@ namespace TPN1_Grupal
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.lblAgregarNombre = new System.Windows.Forms.Label();
+            this.txtBoxAgregar = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.listNombre = new System.Windows.Forms.ListBox();
+            this.listNombreAgregado = new System.Windows.Forms.ListBox();
+            this.btnFlechita = new System.Windows.Forms.Button();
+            this.btnFechitaTodo = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblAgregarNombre
+            // 
+            this.lblAgregarNombre.AutoSize = true;
+            this.lblAgregarNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAgregarNombre.Location = new System.Drawing.Point(127, 42);
+            this.lblAgregarNombre.Name = "lblAgregarNombre";
+            this.lblAgregarNombre.Size = new System.Drawing.Size(143, 16);
+            this.lblAgregarNombre.TabIndex = 0;
+            this.lblAgregarNombre.Text = "Ingrese un Nombre:";
+            // 
+            // txtBoxAgregar
+            // 
+            this.txtBoxAgregar.Location = new System.Drawing.Point(272, 41);
+            this.txtBoxAgregar.Name = "txtBoxAgregar";
+            this.txtBoxAgregar.Size = new System.Drawing.Size(352, 20);
+            this.txtBoxAgregar.TabIndex = 1;
+            this.txtBoxAgregar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBoxAgregar_KeyUp);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(654, 37);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 27);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // listNombre
+            // 
+            this.listNombre.FormattingEnabled = true;
+            this.listNombre.Location = new System.Drawing.Point(130, 129);
+            this.listNombre.Name = "listNombre";
+            this.listNombre.Size = new System.Drawing.Size(217, 264);
+            this.listNombre.TabIndex = 3;
+            // 
+            // listNombreAgregado
+            // 
+            this.listNombreAgregado.FormattingEnabled = true;
+            this.listNombreAgregado.Location = new System.Drawing.Point(488, 129);
+            this.listNombreAgregado.Name = "listNombreAgregado";
+            this.listNombreAgregado.Size = new System.Drawing.Size(217, 264);
+            this.listNombreAgregado.TabIndex = 4;
+            // 
+            // btnFlechita
+            // 
+            this.btnFlechita.Location = new System.Drawing.Point(379, 193);
+            this.btnFlechita.Name = "btnFlechita";
+            this.btnFlechita.Size = new System.Drawing.Size(75, 39);
+            this.btnFlechita.TabIndex = 5;
+            this.btnFlechita.Text = ">";
+            this.btnFlechita.UseVisualStyleBackColor = true;
+            this.btnFlechita.Click += new System.EventHandler(this.btnFlechita_Click);
+            // 
+            // btnFechitaTodo
+            // 
+            this.btnFechitaTodo.Location = new System.Drawing.Point(379, 268);
+            this.btnFechitaTodo.Name = "btnFechitaTodo";
+            this.btnFechitaTodo.Size = new System.Drawing.Size(75, 38);
+            this.btnFechitaTodo.TabIndex = 6;
+            this.btnFechitaTodo.Text = ">>";
+            this.btnFechitaTodo.UseVisualStyleBackColor = true;
+            this.btnFechitaTodo.Click += new System.EventHandler(this.btnFechitaTodo_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // FormEjercicio1
             // 
@@ -37,12 +116,31 @@ namespace TPN1_Grupal
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFechitaTodo);
+            this.Controls.Add(this.btnFlechita);
+            this.Controls.Add(this.listNombreAgregado);
+            this.Controls.Add(this.listNombre);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtBoxAgregar);
+            this.Controls.Add(this.lblAgregarNombre);
             this.Name = "FormEjercicio1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nombres";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAgregarNombre;
+        private System.Windows.Forms.TextBox txtBoxAgregar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ListBox listNombre;
+        private System.Windows.Forms.ListBox listNombreAgregado;
+        private System.Windows.Forms.Button btnFlechita;
+        private System.Windows.Forms.Button btnFechitaTodo;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
--- TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace TPN1_Grupal
 {
@@ -15,6 +16,113 @@ namespace TPN1_Grupal
         public FormEjercicio1()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBoxAgregar.Text.Trim()))
+            {
+                //txtBoxAgregar.BackColor = Color.Red;
+                errorProvider1.SetError(txtBoxAgregar, "Ingrese un nombre válido");
+                btnAgregar.Enabled = false;
+                
+                //MessageBox.Show("¡Debe ingresar un nombre!","ATENCIÓN",MessageBoxButtons.OK,MessageBoxIcon.Exclamation);
+                                                 /// Después de mostrar cartel deja vacío el Text Box).
+                                                /// Más que nada cuando se ingresan sólo espacios en blanco.
+
+                
+            }
+            else
+            {
+                int cantNombres, i;
+                bool repetido = false;
+                cantNombres = listNombre.Items.Count; // Cuenta la cantidad de nombres cargados en el List Box.
+
+                for (i = 0; i < cantNombres; i++)
+                {
+                    // Compara el nombre ingresado en el text box con cada uno de los nombres del list box. Antes pasa toda la cadena de ambos nombres a MAYÚSCULAS.
+                    if (txtBoxAgregar.Text.ToUpper().Trim() == listNombre.Items[i].ToString().ToUpper())
+                    {
+                        MessageBox.Show("El nombre ingresado ya se encuentra en la lista", "ATENCIÓN");
+                        repetido = true;
+                    }
+                }
+                // Si no hay repetido agrega el nombre a la lista.
+                if (repetido == false)
+                {
+                    //txtBoxAgregar.Text.Trim();
+                    listNombre.Items.Add(txtBoxAgregar.Text.Trim());    /// Quita los espacios del inicio y final de la cadena
+                                                                        /// al momento de agregar el nombre al List Box.
+                    txtBoxAgregar.Text = "";                            /// Vuelve a poner en blanco el Text Box
+                }
+
+                //txtBoxAgregar.BackColor = System.Drawing.SystemColors.Control;
+            }
+
+
+
+        }
+
+        /*private void txtBoxAgregar_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255))
+                {
+                    if (e.KeyChar != 32)
+                    {
+                        MessageBox.Show("SOLO LETRAS", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        e.Handled = true;
+                    }
+                }
+        }*/
+
+        private void btnFlechita_Click(object sender, EventArgs e)
+        {
+            if (listNombre.Items.Count > 0)
+            {
+                if (listNombre.SelectedItem == null)
+                {
+                    MessageBox.Show("Seleccione un nombre de la lista izquierda para transferirlo a la derecha!", "ATENCIÓN");
+                }
+                else
+                {
+                    int index = listNombre.SelectedIndex;
+                    listNombreAgregado.Items.Add(listNombre.SelectedItem);
+                    listNombre.Items.RemoveAt(index);
+                }
+            }
+            else
+            {
+                MessageBox.Show("LA LISTA ESTÁ VACÍA. NO SE PUEDEN TRANSFERIR ELEMENTOS", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnFechitaTodo_Click(object sender, EventArgs e)
+        {
+            if(listNombre.Items.Count > 0)
+            {
+                 listNombreAgregado.Items.AddRange(listNombre.Items);
+                 listNombre.Items.Clear();
+            }
+            else
+            {
+                MessageBox.Show("No hay ningun nombre en la lista izquierda para transferirlo a la derecha, Por favor ingrese un nombre!", "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        
+
+        private void txtBoxAgregar_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Regex.IsMatch(txtBoxAgregar.Text.Trim(), "[^a-zA-ZáéíóúÁÉÍÓÚü ]"))
+            {
+                errorProvider1.SetError(txtBoxAgregar, "Debe ingresar un nombre válido");
+                btnAgregar.Enabled = false;
+            }
+            else
+            {
+                errorProvider1.SetError(txtBoxAgregar, "");
+                btnAgregar.Enabled = true;
+            }
         }
     }
 }
