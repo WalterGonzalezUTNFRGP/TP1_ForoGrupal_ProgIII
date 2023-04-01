04ad6ba0d00081ac06d946d4aaa62f61c94c8091
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-03-31 22:16:49 -0300

    Versión 0.19
    Iniciamos con el Ejercicio 3, realicé la primer parte del diseño visual para comenzar con la tarea

--- TPN1_Grupal/TPN1_Grupal/FormEjercicio3.Designer.cs
+++ TPN1_Grupal/TPN1_Grupal/FormEjercicio3.Designer.cs
@@ -29,20 +29,107 @@ namespace TPN1_Grupal
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbFem = new System.Windows.Forms.RadioButton();
+            this.rbMas = new System.Windows.Forms.RadioButton();
+            this.rbCas = new System.Windows.Forms.RadioButton();
+            this.rbSol = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbMas);
+            this.groupBox1.Controls.Add(this.rbFem);
+            this.groupBox1.Location = new System.Drawing.Point(35, 67);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(139, 89);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbSol);
+            this.groupBox2.Controls.Add(this.rbCas);
+            this.groupBox2.Location = new System.Drawing.Point(261, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(143, 89);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Estado Civil";
+            // 
+            // rbFem
+            // 
+            this.rbFem.AutoSize = true;
+            this.rbFem.Location = new System.Drawing.Point(15, 28);
+            this.rbFem.Name = "rbFem";
+            this.rbFem.Size = new System.Drawing.Size(71, 17);
+            this.rbFem.TabIndex = 0;
+            this.rbFem.TabStop = true;
+            this.rbFem.Text = "Femenino";
+            this.rbFem.UseVisualStyleBackColor = true;
+            // 
+            // rbMas
+            // 
+            this.rbMas.AutoSize = true;
+            this.rbMas.Location = new System.Drawing.Point(15, 50);
+            this.rbMas.Name = "rbMas";
+            this.rbMas.Size = new System.Drawing.Size(73, 17);
+            this.rbMas.TabIndex = 1;
+            this.rbMas.TabStop = true;
+            this.rbMas.Text = "Masculino";
+            this.rbMas.UseVisualStyleBackColor = true;
+            // 
+            // rbCas
+            // 
+            this.rbCas.AutoSize = true;
+            this.rbCas.Location = new System.Drawing.Point(12, 28);
+            this.rbCas.Name = "rbCas";
+            this.rbCas.Size = new System.Drawing.Size(61, 17);
+            this.rbCas.TabIndex = 2;
+            this.rbCas.TabStop = true;
+            this.rbCas.Text = "Casado";
+            this.rbCas.UseVisualStyleBackColor = true;
+            // 
+            // rbSol
+            // 
+            this.rbSol.AutoSize = true;
+            this.rbSol.Location = new System.Drawing.Point(12, 50);
+            this.rbSol.Name = "rbSol";
+            this.rbSol.Size = new System.Drawing.Size(58, 17);
+            this.rbSol.TabIndex = 3;
+            this.rbSol.TabStop = true;
+            this.rbSol.Text = "Soltero";
+            this.rbSol.UseVisualStyleBackColor = true;
             // 
             // FormEjercicio3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(445, 529);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Name = "FormEjercicio3";
             this.Text = "Ejercicio3";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbMas;
+        private System.Windows.Forms.RadioButton rbFem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbSol;
+        private System.Windows.Forms.RadioButton rbCas;
     }
 }
