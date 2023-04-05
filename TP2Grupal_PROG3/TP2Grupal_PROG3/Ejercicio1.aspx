74b5215acb4fc10a0461c405d730ca1e52da4170
MNRocha <mnrrocha@hotmail.com>
2023-04-05 02:00:57 -0300

    Version 0.3
    Validación text box cantidades

--- TP2Grupal_PROG3/TP2Grupal_PROG3/Ejercicio1.aspx
+++ TP2Grupal_PROG3/TP2Grupal_PROG3/Ejercicio1.aspx
@@ -15,16 +15,18 @@
             <asp:Label ID="LBLproducto1" runat="server" Text="Ingrese nombre del Producto:"></asp:Label>
             <asp:TextBox ID="TXTproducto1" runat="server"></asp:TextBox>
             <asp:Label ID="LBLcantidad1" runat="server" Text="Cantidad:"></asp:Label>
-            <asp:TextBox ID="TXTcantidad1" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TXTcantidad1" runat="server" AutoPostBack="True" OnTextChanged="TXTcantidad1_TextChanged" TextMode="Number" Width="162px"></asp:TextBox>
         </p>
         <p>
             <asp:Label ID="LBLproducto2" runat="server" Text="Ingrese nombre del Producto:"></asp:Label>
             <asp:TextBox ID="TXTproducto2" runat="server"></asp:TextBox>
             <asp:Label ID="LBLcantidad2" runat="server" Text="Cantidad:"></asp:Label>
-            <asp:TextBox ID="TXTcantidad2" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TXTcantidad2" runat="server" AutoPostBack="True" OnTextChanged="TXTcantidad2_TextChanged" TextMode="Number"></asp:TextBox>
         </p>
         <p>
             <asp:Button ID="BTNgenerar" runat="server" OnClick="BTNgenerar_Click" style="height: 26px" Text="Generar Tabla" />
+        &nbsp;&nbsp;
+            <asp:Label ID="lblVerificacion" runat="server"></asp:Label>
         </p>
         <p>
             <asp:Label ID="LBLtabla" runat="server"></asp:Label>
--- TP2Grupal_PROG3/TP2Grupal_PROG3/Ejercicio1.aspx.cs
+++ TP2Grupal_PROG3/TP2Grupal_PROG3/Ejercicio1.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Drawing;
 
 namespace TP2Grupal_PROG3
 {
@@ -16,27 +17,73 @@ namespace TP2Grupal_PROG3
 
         protected void BTNgenerar_Click(object sender, EventArgs e)
         {
-            int cant1 = int.Parse(TXTcantidad1.Text);
-            int cant2 = int.Parse(TXTcantidad2.Text);
-            int resultado = cant1 + cant2;
-            string tabla = "<table border='1'>";
-            tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
-            tabla += "<tr>";
-            tabla += "<td>" + TXTproducto1.Text + "</td>";
-            tabla += "<td>" + TXTcantidad1.Text + "</td>";
-            tabla += "</tr>";
-            tabla += "<tr>";
-            tabla += "<td>" + TXTproducto2.Text + "</td>";
-            tabla += "<td>" + TXTcantidad2.Text + "</td>";
-            tabla += "</tr>";
-            tabla += "<tr>";
-            tabla += "<th>Total</th>";
-            tabla += "<td>"+ resultado+"</td>";
-            tabla += "</tr>";
-            tabla += "</table>";
-            LBLtabla.Text = tabla;
+            if (TXTcantidad1.Text != "" && TXTcantidad2.Text != "")
+            {
+                int cant1 = int.Parse(TXTcantidad1.Text);
+                int cant2 = int.Parse(TXTcantidad2.Text);
+                int resultado = cant1 + cant2;
+                string tabla = "<table border='1'>";
+                tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
+                tabla += "<tr>";
+                tabla += "<td>" + TXTproducto1.Text + "</td>";
+                tabla += "<td>" + TXTcantidad1.Text + "</td>";
+                tabla += "</tr>";
+                tabla += "<tr>";
+                tabla += "<td>" + TXTproducto2.Text + "</td>";
+                tabla += "<td>" + TXTcantidad2.Text + "</td>";
+                tabla += "</tr>";
+                tabla += "<tr>";
+                tabla += "<th>Total</th>";
+                tabla += "<td>" + resultado + "</td>";
+                tabla += "</tr>";
+                tabla += "</table>";
+                LBLtabla.Text = tabla;
+                TXTproducto1.Text = "";
+                TXTproducto2.Text = "";
+                TXTcantidad1.Text = "";
+                TXTcantidad2.Text = "";
+                lblVerificacion.Text = "";
+            }
+            else
+            {
+                lblVerificacion.ForeColor = Color.Red;
+                lblVerificacion.Text = "Debe completar todos los campos";
+                //TXTcantidad1.BackColor = Color.Red;
+                //TXTcantidad2.BackColor = Color.Red;
+                if (TXTcantidad1.Text == "")
+                {
+                    TXTcantidad1.BackColor = Color.Red;
+                }
+
+                if (TXTcantidad2.Text == "")
+                {
+                    TXTcantidad2.BackColor = Color.Red;
+                }
+            }
         }
-            
-        
+
+        protected void TXTcantidad1_TextChanged(object sender, EventArgs e)
+        {
+            if (TXTcantidad1.Text != "")
+            {
+                TXTcantidad1.BackColor = Color.White;
+                if(TXTcantidad2.Text != "")
+                {
+                    lblVerificacion.Text = "";
+                }
+            }
+        }
+
+        protected void TXTcantidad2_TextChanged(object sender, EventArgs e)
+        {
+            if (TXTcantidad2.Text != "")
+            {
+                TXTcantidad2.BackColor  = Color.White;
+                if (TXTcantidad1.Text != "")
+                {
+                    lblVerificacion.Text = "";
+                }
+            }
+        }
     }
 }
