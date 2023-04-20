d2c3af8957f30275b4b5fcca96bfb575e33ecd62
Enzo Matías Nieto <enzonieto92@hotmail.com>
2023-04-20 13:56:02 -0300

    Conexión a Base de Datos Local

--- TPN4/TPN4/WebForm1.aspx.cs
+++ TPN4/TPN4/WebForm1.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
 
 namespace TPN4
 {
@@ -11,7 +13,21 @@ namespace TPN4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string rutaViajesBD = "Data Source = localhost\\sqlexpress; Initial Catalog = Viajes; Integrated Security = True";
+            if (IsPostBack == false)
+            {
+                SqlConnection cn = new SqlConnection(@"Data Source=localhost\sqlexpress;Initial Catalog=Viajes;Integrated Security=True");
+                cn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select * from Provincias", cn);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    ddlProvincias.Items.Add(dr["NombreProvincia"].ToString());
+                }
+
+                cn.Close();
+            }
         }
     }
 }
