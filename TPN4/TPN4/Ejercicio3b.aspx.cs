3e09131e8d2c897a769b6298196f6718f8b29bca
axel mazo <axel.98.mazo@gmail.com>
2023-04-25 19:54:20 -0300

    Conexion de base de datos Ej3

--- TPN4/TPN4/Ejercicio3b.aspx.cs
+++ TPN4/TPN4/Ejercicio3b.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace TPN4
 {
@@ -11,7 +13,27 @@ namespace TPN4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                SqlConnection cn = new SqlConnection("Data Source = localhost\\sqlexpress; Initial Catalog = Libreria; Integrated Security = True");
 
+                cn.Open();
+
+                string consulta;
+
+                consulta = "SELECT * FROM Libros";
+                SqlCommand cmd = new SqlCommand(consulta, cn);
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                if (rdr.HasRows)
+                {
+                    grdVConsultaLibros.DataSource = rdr;
+                    grdVConsultaLibros.DataBind();
+                }
+
+                cn.Close();
+            }
         }
     }
 }
